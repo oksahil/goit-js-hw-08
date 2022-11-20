bc46bd10b472bb66882da3b481b34c0c69a52b11
oksahil <oksana.khylyk@gmail.com>
2022-11-20 15:25:11 +0100

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const throttle = require('lodash.throttle');
+
+
+player.on('timeupdate', throttle(onTimeUpdateSave, 1000));
+player.pause('timeupdate', pauseTimeUpdateSave);
+player.setCurrentTime(timeUpdateFetch());
+
+function timeUpdateFetch() {
+    const timeSecond = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+    console.log(timeSecond);
+    return timeSecond;
+}
+
+function onTimeUpdateSave(timeObject) {
+console.log(timeObject.seconds);
+localStorage.setItem('videoplayer-current-time', JSON.stringify(timeObject.seconds));
+}
+
+function pauseTimeUpdateSave(timeObject) {
+console.log(timeObject.seconds);
+localStorage.setItem('videoplayer-current-time', JSON.stringify(timeObject.seconds));
+}
 
